2ef76fefa341d55bb13ef12bd684aa3ea1aed0b0
sselvag <swaathy.selva@gmail.com>
2022-07-23 00:52:40 -0400

    routes to html

---
+++ Develop/routes/routes.js
@@ -0,0 +1,14 @@
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = app; 
+
