3bae5a148c69434bb693b600b872e6f2cb6ead00
Swaathy <64749332+sselvag@users.noreply.github.com>
2022-07-27 03:53:49 -0400

    fixed routes

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const APIroutes = require("./routes/APIroutes");
 const routes = require("./routes/routes");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', routes);
 app.use(express.static('public'));
+app.use('/api', APIroutes);
+app.use('/', routes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
