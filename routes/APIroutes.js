98a96c4b8217866d1e1db92483c9c6254fe79375
Swaathy <64749332+sselvag@users.noreply.github.com>
2022-07-27 03:59:55 -0400

    troubleshooting

--- routes/APIroutes.js
+++ routes/APIroutes.js
@@ -1,27 +1,26 @@
-const db = require("../db/db.json");
+let db = require("../db/db.json");
 const fs = require("fs");
-const express = require("express");
-const app = express();
+const app = require("express").Router();
 
-app.get("/notes", function(req, res) {
-    db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.json(db);
-});
-
-app.post("/notes", function(req, res) {
-    let newNote = {
-        id: Math.floor(Math.random() * 100),
-        title: req.body.title,
-        text: req.body.text
-    };
-    db.push(newNote);
-    fs.writeFileSync("./db/db.json", JSON.stringify(db), function(err, res) {
-        if (err) {
-            throw err;
-        }
+    app.get("/notes", function(req, res) {
+        db = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        res.json(db);
     });
-    res.json(db);
-});
+
+    app.post("/notes", function(req, res) {
+        let newNote = {
+            id: Math.floor(Math.random() * 100),
+            title: req.body.title,
+            text: req.body.text
+        };
+        db.push(newNote);
+        fs.writeFileSync("./db/db.json", JSON.stringify(db), function(err, res) {
+            if (err) {
+                throw err;
+            }
+        });
+        res.json(db);
+    });
 
     app.delete("/notes/:id", function(req, res) {
         let undeletedNotes = [];
@@ -39,5 +38,4 @@ app.post("/notes", function(req, res) {
         console.log("Delete", db);
         res.json(db);
     });
-
 module.exports = app;
